7adc600c442f86d7395d62fa0a12cc6dbf676a1d
herrand15 <20165239@ce.pucmm.edu.do>
2018-03-12 16:55:12 -0400

    all added

--- src/BoilerPlate/Matrix4.cpp
+++ src/BoilerPlate/Matrix4.cpp
@@ -1,5 +1,5 @@
 #include "Matrix4.hpp"
-
+#include <iostream>
 #define MATRIX_SIZE 4
 
 Matrix4::Matrix4()
@@ -7,124 +7,143 @@ Matrix4::Matrix4()
 	SetIdentity();
 }
 
-
 Matrix4::~Matrix4()
 {
+
 }
 
 void Matrix4::SetIdentity()
 {
 	//RESET VALUES (0)
-	MATRIX = std::vector<std::vector <float> >(4, std::vector <float>(4, 0.0f));
-
+	MATRIX = std::vector<float>(16, 0.0f);
 	//SET ONES ON MAIN DIAGONAL
 	for (int i = 0; i < MATRIX_SIZE; i++)
-		MATRIX[i][i] = 1.0f;
+		MATRIX[i * 4 + i] = 1.0f;
 }
 
-
-std::vector <std::vector <float> > Matrix4::Get() {
+std::vector <float> Matrix4::Get() {
 	return MATRIX;
 }
 
-std::vector <std::vector <float> > Matrix4::GetTransposed() {
-	std::vector <std::vector <float> > transposedElements; 
+std::vector <float> Matrix4::GetTransposed() {
+	std::vector <float> transposedElements;
 	for (int i = 0; i < MATRIX_SIZE; i++) {
 		for (int j = 0; j < MATRIX_SIZE; j++) {
-			transposedElements[i][j] = MATRIX[j][i];
-
-
-
+			transposedElements[i * 4 + j] = MATRIX[j * 4 + i];
 		}
 	}
-
 	return transposedElements;
-
 }
 
 Matrix4 Matrix4::operator+(Matrix4& rhs) {
-	for (int i = 0; i < MATRIX_SIZE; i++) {
-		for (int j = 0; j < MATRIX_SIZE; j++) {
-			MATRIX[i][j] += rhs.Get()[i][j];
-
-		}
-	}
+	for (int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++) MATRIX[i] += rhs.MATRIX[i];
 	return *this;
 }
-
 Matrix4 Matrix4::operator-(Matrix4& rhs) {
-	for (int i = 0; i < MATRIX_SIZE; i++) {
-		for (int j = 0; j < MATRIX_SIZE; j++) {
-			MATRIX[i][j] -= rhs.Get()[i][j];
-
-		}
-	}
+	for (int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++) MATRIX[i] -= rhs.MATRIX[i];
 	return *this;
 }
-
 Matrix4 Matrix4::operator*(Matrix4& rhs) {
 	for (int i = 0; i < MATRIX_SIZE; i++) {
 		for (int j = 0; i < MATRIX_SIZE; i++) {
 			for (int k = 0; k < MATRIX_SIZE; k++) {
 				MATRIX[i][k] *= rhs.Get()[k][j];
-
 			}
 		}
 	}
 	return *this;
 }
-
 Matrix4 Matrix4::operator/(Matrix4& rhs) {
 	*this = *this * rhs.GetInverse();
 	return *this;
 }
-
+Matrix4 Matrix4::operator=(std::vector<float> elements)
+{
+	MATRIX = elements;
+}
 
 float Matrix4::CalculateDeterminant() {
 	float determinant;
 	int i;
 	for (i = 0; i < MATRIX_SIZE; i++)
 		determinant = determinant + (MATRIX[0][i] * (MATRIX[1][(i + 1) % 3] * MATRIX[2][(i + 2) % 3] - MATRIX[1][(i + 2) % 3] * MATRIX[2][(i + 1) % 3]));
-
 	return determinant;
 }
-
 Matrix4 Matrix4::GetInverse() {
 	Matrix4 inverse;
 	int i, j;
-
 	for (i = 0; i < MATRIX_SIZE; i++) {
 		for (j = 0; j < 3; j++)
-			inverse.Get()[i][j]=  ((MATRIX[(j + 1) % 3][(i + 1) % 3] * MATRIX[(j + 2) % 3][(i + 2) % 3]) - (MATRIX[(j + 1) % 3][(i + 2) % 3] * MATRIX[(j + 2) % 3][(i + 1) % 3])) / this->CalculateDeterminant();
-
+			inverse.Get()[i][j] = ((MATRIX[(j + 1) % 3][(i + 1) % 3] * MATRIX[(j + 2) % 3][(i + 2) % 3]) - (MATRIX[(j + 1) % 3][(i + 2) % 3] * MATRIX[(j + 2) % 3][(i + 1) % 3])) / this->CalculateDeterminant();
 	}
-
 	return inverse;
 }
-
-
-
 float Matrix4::GetAngle() {
 	float roll, yaw, pitch;
 	float angle;
 	if (MATRIX[0][0] == 1.0f)
 	{
+		roll = 0.0f;
 		yaw = atan2f(MATRIX[0][2], MATRIX[2][3]);
-		pitch = 0;
-		roll = 0;
+		pitch = 0.0f;
+
 	}
-	else if (MATRIX[0][0] == -1.0f)
+	else if (MATRIX[0] == -1.0f)
 	{
-		yaw = atan2f(MATRIX[0][2], MATRIX[2][3]);
-		pitch = 0;
-		roll = 0;
+		roll = 0.0f;
+		yaw = atan2f(MATRIX[8], MATRIX[10]);
+		pitch = 0.0f;
+
 	}
 	else
 	{
-		yaw = atan2(-MATRIX[3][0], MATRIX[0][0]);
-		pitch = asin(MATRIX[1][0]);
-		roll = atan2(-MATRIX[1][2], MATRIX[1][1]);
-	}
+		roll = atan2(-MATRIX[9], MATRIX[5]);
+		yaw = atan2(-MATRIX[3], MATRIX[0]);
+		pitch = asin(MATRIX[1]);
 
+	}
+}
+float Matrix4::GetValue(int row, int column)
+{
+	return MATRIX[column * 4 + row];
+}
+void Matrix4::Rotate(float angle, Vector3) {
+}
+void Matrix4::Translate(Vector3 translationVector) {
+	for (int i = 0; i < MATRIX_SIZE; i++) {
+
+		MATRIX[i][0] * translationVector.x;
+
+		MATRIX[i][1] * translationVector.y;
+
+		MATRIX[i][2] * translationVector.z;
+
+		MATRIX[i][3] * 1;
+
+	}
 }
 
+Vector3 Matrix4::transform(Vector3 point)
+{
+	Matrix4 MPoint = Matrix4();
+	MPoint.MATRIX[12] = point.x;
+	MPoint.MATRIX[13] = point.y;
+	MPoint.MATRIX[14] = point.z;
+	return Vector3(MPoint.MATRIX[12], MPoint.MATRIX[13], MPoint.MATRIX[14]);
+}
+
+
+Vector4 Matrix4::transform(Vector4 point)
+{
+	Matrix4 MPoint = Matrix4();
+	MPoint.MATRIX[12] = point.x;
+	MPoint.MATRIX[13] = point.y;
+	MPoint.MATRIX[14] = point.z;
+	MPoint.MATRIX[15] = point.z;
+	return Vector4(MPoint.MATRIX[12], MPoint.MATRIX[13], MPoint.MATRIX[14], MPoint.MATRIX[15]);
+}
+std::ostream & operator<<(std::ostream & stream, const Matrix4 & matrix)
+{
+	for (int i = 0; i < MATRIX_SIZE; i++) {
+		std::cout << matrix.MATRIX[i] << " " << matrix.MATRIX[i + 4] << " " << matrix.MATRIX[i + 8] << " " << matrix.MATRIX[i + 12] << '\n';
+	}
--- src/BoilerPlate/Matrix4.hpp
+++ src/BoilerPlate/Matrix4.hpp
@@ -1,30 +1,30 @@
 #pragma once
-
 #ifndef MATRIX4_HPP_
 #define MATRIX4_HPP_
-
 #include <vector>
-
+#include "Vector3.hpp"
+#include"Vector4.hpp"
 class Matrix4
 {
 public:
 	Matrix4();
 	~Matrix4();
-	std::vector <std::vector <float> > Get();
+	std::vector <std::vector <float> > * Get();
 	std::vector <std::vector <float> > GetTransposed();
 	Matrix4 operator+ (Matrix4& rhs);
 	Matrix4 operator- (Matrix4& rhs);
 	Matrix4 operator* (Matrix4& rhs);
 	Matrix4 operator/ (Matrix4& rhs);
+	Matrix4 operator=(std::vector <float> elements);
 	float CalculateDeterminant();
 	Matrix4 GetInverse();
-	float GetAngle();
-
+	Vector3 GetAngle();
+	void Rotate(float angle, Vector3);
+	void Translate(Vector3 translationVector);
+	Vector3 transform(Vector3 point);
+	Vector4 transform(Vector4 point);
 	void SetIdentity();
-
 private:
-	std::vector <std::vector <float> > MATRIX;
-	
+	std::vector <float> MATRIX;
 };
-
 #endif /* MATRIX4_HPP_ */
